50d8de88a12fd7f9161e88ee3d4b0081f5993ca7
DucThang22 <50301049+DucThang22@users.noreply.github.com>
2024-01-09 22:00:49 +0700

    update

--- app/src/main/java/com/voiceeffects/supereffect/convertvideotomp3/data/repository/VideoRepository.kt
+++ app/src/main/java/com/voiceeffects/supereffect/convertvideotomp3/data/repository/VideoRepository.kt
@@ -1,7 +1,11 @@
 package com.voiceeffects.supereffect.convertvideotomp3.data.repository
 
+import android.content.Context
+import android.provider.MediaStore
 import com.voiceeffects.supereffect.convertvideotomp3.data.database.dao.VideoDao
 import com.voiceeffects.supereffect.convertvideotomp3.data.database.entity.VideoGalleryEntity
+import com.voiceeffects.supereffect.convertvideotomp3.data.model.VideoModel
+import java.io.File
 import javax.inject.Inject
 
 class VideoRepository @Inject constructor(
@@ -16,4 +20,57 @@ class VideoRepository @Inject constructor(
     suspend fun updateVideo(videoGalleryEntity: VideoGalleryEntity) = videoDao.updateVideo(videoGalleryEntity)
 
     suspend fun getAllVideo() = videoDao.getAllVideo()
+
+    fun syncVideoFromExternalStorage(context: Context): MutableList<VideoModel> {
+        val listVideo: MutableList<VideoModel> = mutableListOf()
+        context.contentResolver.query(
+            MediaStore.Video.Media.EXTERNAL_CONTENT_URI,
+            arrayOf(
+                MediaStore.Video.VideoColumns.DATA,
+                MediaStore.Video.VideoColumns.TITLE,
+                MediaStore.Video.VideoColumns.DURATION,
+                MediaStore.Video.VideoColumns.SIZE,
+                MediaStore.Video.VideoColumns.ARTIST,
+                MediaStore.Video.VideoColumns.ALBUM
+            ), null, null, null
+        )?.use { cursor ->
+            val columnPath = cursor.getColumnIndex(MediaStore.Video.VideoColumns.DATA)
+            val columnTitle = cursor.getColumnIndex(MediaStore.Video.VideoColumns.TITLE)
+            val columnDuration = cursor.getColumnIndex(MediaStore.Video.VideoColumns.DURATION)
+            val columnSize = cursor.getColumnIndex(MediaStore.Video.VideoColumns.SIZE)
+            val columnArtist = cursor.getColumnIndex(MediaStore.Video.VideoColumns.ARTIST)
+            val columnAlbum = cursor.getColumnIndex(MediaStore.Video.VideoColumns.ALBUM)
+            while (cursor.moveToNext()) {
+                if (columnPath > -1) {
+                    val path = cursor.getString(columnPath)
+                    if (path != null && !path.startsWith(".")) {
+                        val file = File(path)
+                        if (file.isFile && !file.isHidden) {
+                            val nameFileVideo = cursor.getString(columnTitle) ?: file.name
+                            val nameSize = cursor.getLong(columnSize)
+                            val nameDuration = cursor.getLong(columnDuration)
+                            val nameArtist = cursor.getString(columnArtist)
+                            val nameAlbum = cursor.getString(columnAlbum)
+                            if (nameFileVideo.startsWith(".")) continue
+                            val duration = cursor.getLong(columnDuration)
+                            if (duration >= 1000) {
+                                listVideo.add(
+                                    VideoModel(
+                                        nameFileVideo,
+                                        nameDuration,
+                                        path,
+                                        nameSize,
+                                        nameArtist,
+                                        nameAlbum
+                                    )
+                                )
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        listVideo.sortWith(compareBy { it.title.uppercase() })
+        return listVideo
+    }
 }
